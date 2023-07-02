c5bf71d1aeabe6a454aa5c0ad800c143c6b44470
Menderes Özkan <105667249+mndozkn@users.noreply.github.com>
2023-07-03 00:25:03 +0300

    Update FavoritesClient.tsx

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -18,16 +18,17 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
 			<Heading title="Favorites" subtitle="List of places you favorited!" />
 			<div
 				className="
-          mt-10
-          grid 
-          grid-cols-1 
-          sm:grid-cols-2 
-          md:grid-cols-3 
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        ">
+			          mt-10
+			          grid 
+			          grid-cols-1 
+			          sm:grid-cols-2 
+			          md:grid-cols-3 
+			          lg:grid-cols-4
+			          xl:grid-cols-5
+			          2xl:grid-cols-6
+			          gap-8
+			        "
+			>
 				{listings.map((listing: any) => (
 					<ListingCard
 						currentUser={currentUser}
