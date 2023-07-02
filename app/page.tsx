d727e537a875d5f0755e3512f97d5f4006576ed0
Menderes Özkan <105667249+mndozkn@users.noreply.github.com>
2023-07-03 00:21:52 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,16 +27,17 @@ const Home = async ({ searchParams }: HomeProps) => {
 			<Container>
 				<div
 					className="
-            pt-24
-            grid 
-            grid-cols-1 
-            sm:grid-cols-2 
-            md:grid-cols-3 
-            lg:grid-cols-4
-            xl:grid-cols-5
-            2xl:grid-cols-6
-            gap-8
-          ">
+				            pt-24
+				            grid 
+				            grid-cols-1 
+				            sm:grid-cols-2 
+				            md:grid-cols-3 
+				            lg:grid-cols-4
+				            xl:grid-cols-5
+				            2xl:grid-cols-6
+				            gap-8
+				          "
+				>
 					{listings.map((listing: any) => (
 						<ListingCard
 							currentUser={currentUser}
