a3369147b99554682809722c5dc643f78e135a47
Menderes Özkan <105667249+mndozkn@users.noreply.github.com>
2023-07-03 00:27:19 +0300

    Update CategoryBox.tsx

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -50,19 +50,20 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
 		<div
 			onClick={handleClick}
 			className={`
-                flex
-                flex-col
-                items-center
-                justify-center
-                gap-2
-                p-3
-                border-b-2
-                hover:text-neutral-800
-                transition
-                cursor-pointer
-                ${selected ? "border-b-neutral-800" : "border-transparent"}
-                ${selected ? "text-neutral-800" : "text-neutral-500"}
-            `}>
+		                flex
+		                flex-col
+		                items-center
+		                justify-center
+		                gap-2
+		                p-3
+		                border-b-2
+		                hover:text-neutral-800
+		                transition
+		                cursor-pointer
+		                ${selected ? "border-b-neutral-800" : "border-transparent"}
+		                ${selected ? "text-neutral-800" : "text-neutral-500"}
+		            `}
+		>
 			<Icon size={26} />
 			<div className="font-medium text-sm">{label}</div>
 		</div>
