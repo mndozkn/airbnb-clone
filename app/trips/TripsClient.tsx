c32fbf9dfb71c883d9dc78fbc52bcd290cf5acd0
Menderes Özkan <105667249+mndozkn@users.noreply.github.com>
2023-07-03 00:22:53 +0300

    Update TripsClient.tsx

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -52,16 +52,17 @@ const TripsClient: React.FC<TripsClientProps> = ({
 			/>
 			<div
 				className="
-                    mt-10
-                    grid
-                    grid-cols-1
-                    sm:grid-cols-2
-                    md:grid-cols-3
-                    lg:grid-cols-4
-                    xl:grid-cols-5
-                    2xl:grid-cols-6
-                    gap-8
-                ">
+		                    mt-10
+		                    grid
+		                    grid-cols-1
+		                    sm:grid-cols-2
+		                    md:grid-cols-3
+		                    lg:grid-cols-4
+		                    xl:grid-cols-5
+		                    2xl:grid-cols-6
+		                    gap-8
+		                "
+			>
 				{reservations.map(reservation => (
 					<ListingCard
 						key={reservation.id}
