7aa59542812efb77b94a536effd2674e3cd6c0d5
Menderes Özkan <105667249+mndozkn@users.noreply.github.com>
2023-07-03 00:33:34 +0300

    Update ListingReservation.tsx

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -27,15 +27,16 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
 	return (
 		<div
 			className="
-      bg-white 
-        rounded-xl 
-        border-[1px]
-      border-neutral-200 
-        overflow-hidden
-      ">
+			        bg-white 
+			        rounded-xl 
+			        border-[1px]
+			        border-neutral-200 
+			        overflow-hidden
+			      "
+		>
 			<div
-				className="
-      flex flex-row items-center gap-1 p-4">
+				className="flex flex-row items-center gap-1 p-4"
+			>
 				<div className="text-2xl font-semibold">$ {price}</div>
 				<div className="font-light text-neutral-600">night</div>
 			</div>
@@ -52,14 +53,15 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
 			<hr />
 			<div
 				className="
-          p-4 
-          flex 
-          flex-row 
-          items-center 
-          justify-between
-          font-semibold
-          text-lg
-        ">
+			          p-4 
+			          flex 
+			          flex-row 
+			          items-center 
+			          justify-between
+			          font-semibold
+			          text-lg
+			        "
+			>
 				<div>Total</div>
 				<div>$ {totalPrice}</div>
 			</div>
