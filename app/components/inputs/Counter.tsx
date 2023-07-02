1a7a596bc206ea3826851ff4d1ceeb24ddf6b5e0
Menderes Özkan <105667249+mndozkn@users.noreply.github.com>
2023-07-03 00:35:26 +0300

    Update Counter.tsx

--- app/components/inputs/Counter.tsx
+++ app/components/inputs/Counter.tsx
@@ -38,38 +38,40 @@ const Counter: React.FC<CounterProps> = ({
 				<div
 					onClick={onReduce}
 					className="
-                        w-10
-                        h-10
-                        rounded-full
-                        border-[1px]
-                        border-neutral-400
-                        flex
-                        items-center
-                        justify-center
-                        text-neutral-600
-                        cursor-pointer
-                        hover:opacity-80
-                        transition
-                    ">
+			                        w-10
+			                        h-10
+			                        rounded-full
+			                        border-[1px]
+			                        border-neutral-400
+			                        flex
+			                        items-center
+			                        justify-center
+			                        text-neutral-600
+			                        cursor-pointer
+			                        hover:opacity-80
+			                        transition
+			                    "
+				>
 					<AiOutlineMinus />
 				</div>
 				<div className="font-light text-xl text-neutral-600">{value}</div>
 				<div
 					onClick={onAdd}
 					className="
-                        w-10
-                        h-10
-                        rounded-full
-                        border-[1px]
-                        border-neutral-400
-                        flex
-                        items-center
-                        justify-center
-                        text-neutral-600
-                        cursor-pointer
-                        hover:opacity-80
-                        transition
-                    ">
+			                        w-10
+			                        h-10
+			                        rounded-full
+			                        border-[1px]
+			                        border-neutral-400
+			                        flex
+			                        items-center
+			                        justify-center
+			                        text-neutral-600
+			                        cursor-pointer
+			                        hover:opacity-80
+			                        transition
+			                    "
+				>
 					<AiOutlinePlus />
 				</div>
 			</div>
